92c2cb0f96c210feff6d706be01bfce099dc9328
marcherz <marchernandezgar@gmail.com>
2025-08-20 13:18:44 -0700

    Fix priceIds

--- frontend/web/src/services/stripeService.ts
+++ frontend/web/src/services/stripeService.ts
@@ -84,16 +84,16 @@ class StripeService {
    */
   getPredefinedPrices(): Record<number, string> {
     return {
-      1: 'price_1RgtxfQUJIiEzpqAMBbszFOi',
-      3: 'price_1RgtzrQUJIiEzpqAK2EhWQgm',
-      5: 'price_1Rgu00QUJIiEzpqAWsBcqkCR',
-      7: 'price_1Rgu07QUJIiEzpqAK8bP6v6i',
-      10: 'price_1Rgu0LQUJIiEzpqAo64ycGND',
-      15: 'price_1Rgu0TQUJIiEzpqAYNMDauNP',
-      20: 'price_1Rgu0aQUJIiEzpqAlvTbF9vi',
-      25: 'price_1Rgu0nQUJIiEzpqAUsEuvUXD',
-      50: 'price_1Rgu0tQUJIiEzpqANcva8DeM',
-      100: 'price_1Rgu19QUJIiEzpqAGzzClTwi',
+      1: 'price_1RxN2TE9G1zq8oDPMvKNUz0G',
+      3: 'price_1RxN8jE9G1zq8oDPD0fUeLxr',
+      5: 'price_1RxNBOE9G1zq8oDPuaVBJoIT',
+      7: 'price_1RxNGEE9G1zq8oDPHtvRI7YU',
+      10: 'price_1RxNIME9G1zq8oDPoiegdaTA',
+      15: 'price_1RxNKnE9G1zq8oDPqfgrXn71',
+      20: 'price_1RxNLsE9G1zq8oDPW0hUmih7',
+      25: 'price_1RxNMuE9G1zq8oDP6RXNQ5BL',
+      50: 'price_1RxNO8E9G1zq8oDPab27hSbF',
+      100: 'price_1RxNPUE9G1zq8oDPe3qa53V1',
     };
   }
 
